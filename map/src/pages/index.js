f0f9a8bd7c2e3fd65538beeb4fd896185c07d4bb
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-04-03 16:27:50 -0500

    added layer to map

--- map/src/pages/index.js
+++ map/src/pages/index.js
@@ -33,7 +33,31 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response;
-    console.log("response", response);
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
+            ...country
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [lng, lat]
+          }
+        };
+      })
+    };
+
+    const geoJsonLayers = new L.GeoJSON(geoJson);
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
